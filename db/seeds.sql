2b05196d5ab5d3da8a0f71ad8f9fd611839014fd
KN <knny.nguyen@gmail.com>
2018-10-24 19:48:59 -0400

    almost

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers 
+  (name, eaten)
+VALUES 
+  ("California Burger", false),
+  ("Double Bacon Cheeseburger", false),
+  ("Triple Veggieburger", true),
+  ("Cripsy Potato Cod Slider", true);
